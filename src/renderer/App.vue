b55d80cfca1b9be0e9052a85f90c07a4453beef8
cyper <uniquejava@gmail.com>
2022-04-01 14:45:58 +0800

    Initial commit

---
+++ src/renderer/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <hello></hello>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Hello from './components/Hello.vue'
+import { ipcRenderer } from './electron'
+
+export default defineComponent({
+  name: 'app',
+  components: {
+    Hello,
+  },
+  setup() {
+    ipcRenderer.send('message', 'Hello from App.vue!');
+  },
+})
+</script>
